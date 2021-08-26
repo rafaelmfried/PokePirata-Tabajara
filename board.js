a3dc43e64dad1e9ffaa3c35defb5270bc524316e
Rafael Friederick <rafael.mf.documents@gmail.com>
2021-08-26 03:04:04 -0300

    Finalizado engine de geracao de assets de terreno

--- board.js
+++ board.js
@@ -21,17 +21,93 @@ const fillLandscapeSeeds = (maxSeeds) => {
   const seeds = [];
   do {
     const newRandomSeed = {};
-    newRandomSeed.line = Math.ceil(Math.random() * BOARDSIZE);
-    newRandomSeed.colunm = Math.ceil(Math.random() * BOARDSIZE);
+    newRandomSeed.line = Math.ceil(Math.random() * BOARDSIZE - 1);
+    newRandomSeed.column = Math.ceil(Math.random() * BOARDSIZE - 1);
     if (!seeds.includes(newRandomSeed)) 
       seeds.push(newRandomSeed);
   } while(seeds.length < maxSeeds);
   return seeds;
 };
 
-const genLandscape = () => {
-  const landscapeSeeds = fillLandscapeSeeds(BOARDSIZE/2);
-  console.log(landscapeSeeds);
+const expansionFillLandscapeSeed = (seeds, percentage) => {
+  const completeLandscape = [];
+  
+  if (percentage < 1)
+    return completeLandscape;
+    
+  const includeLandPixel = (seed) => {
+    for (let line = seed.line - 1; line <= seed.line + 1; line += 1) {
+      for (let column = seed.column - 1; column <= seed.column + 1; column += 1) {
+        if (((Math.random() * 100) <= percentage)
+          && (line < BOARDSIZE) && (column < BOARDSIZE) && (line >= 0) && (column >= 0)) {
+          const newFillLandscape = {
+            'line': line,
+            'column': column,
+          };
+          completeLandscape.push(newFillLandscape);
+        }
+      }
+    }
+  };
+    seeds.length > 1 ? seeds.map((seed) => {
+    includeLandPixel(seed)
+  })
+  : includeLandPixel(seeds);
+  return [...completeLandscape, ...expansionFillLandscapeSeed(completeLandscape, percentage/100)];
 };
 
-genLandscape();
+const genLandscape = (probability, expansion) => {
+  const landscapeSeeds = fillLandscapeSeeds((probability/100) * (BOARDSIZE**2));
+  return landscapeSeeds.reduce((landscape, seed) => {
+    const genLandscapeBySeed = expansionFillLandscapeSeed(seed, expansion);
+    genLandscapeBySeed.map((landscapeSeed) => {
+      if (!landscape.includes(landscapeSeed))
+        landscape.push(landscapeSeed)
+      });
+      return landscape;
+  }, []);
+};
+
+const genBoardLandscape = (assets) => {
+  return assets.reduce((landscape, { name, parameters }) => {
+    const probability = parameters[0];
+    const expansion = parameters[1];
+    genLandscape(probability, expansion).forEach(({ line, column}) => {
+      const newBoardLandscapePixel = {
+        'name': name,
+        'id': `L,${line},C,${column}`,
+      };
+      if (!landscape.some(({ id }) => id === newBoardLandscapePixel.id))
+        landscape.push(newBoardLandscapePixel);
+    });
+    return landscape;
+  }, []);
+};
+
+const test = genBoardLandscape([ {
+  'name': 'garden', 
+  'parameters': ([15, 100]),
+ },
+{
+  'name': 'watter',
+  'parameters': ([80, 100]),
+},
+{
+  'name': 'rock',
+  'parameters': ([10, 20]),
+},
+{
+  'name': 'sand',
+  'parameters': ([10, 20]),
+},
+{
+  'name': 'vulkan',
+  'parameters': ([10, 20]),
+} ]);
+
+console.log(test);
+
+test.forEach((pixel) => {
+  const block = document.getElementById(`${pixel.id}`);
+  block.className = `pixel-board ${pixel.name}`;
+});
